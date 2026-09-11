d8111c21887ee276adbb1bee3137f2395b12444f
Prajwal Gowda <prajwalgowda010@gmail.com>
2026-09-11 16:04:58 +0530

    Configure ALLOWED_HOSTS through environment variable

--- MetaNest_config/settings.py
+++ MetaNest_config/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.getenv(
+        "ALLOWED_HOSTS",
+        "localhost,127.0.0.1"
+    ).split(",")
+    if host.strip()
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
